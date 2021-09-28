bd746d82609bd94a28efc4d1dd626633876f0333
rawnawk <alivexhd@gmail.com>
2021-09-28 14:00:39 +0530

    transfer

---
+++ src/c/Header.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react'
+
+import { Link } from "gatsby"
+
+export default function Header() {
+    const [mobile, setMobile] = useState(false)
+    
+    return (
+        <header>
+            <nav className="md:hidden">
+                <div className="flex items-center justify-between px-4 py-5 border-b">
+                    <div>
+                        <a href="/"><h1>Cyberflix</h1></a>
+                    </div>
+                    <button onClick={()=>setMobile(!mobile)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>
+                    </button>
+                </div>
+                <div className={`text-center mobile-nav ${mobile?'block':'hidden'}`}>
+                    <ul>
+                        <li>
+                            <Link to="/download/">Download</Link>
+                        </li>
+                        <li>
+                            <Link to="/firestick/">Firestick</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <nav className="hidden md:block">
+                <div className="flex items-center justify-between px-4 py-5 border-b">
+                    <div>
+                        <a href="/"><h1>Cyberflix</h1></a>
+                    </div>
+                    <div className={`text-center space-x-6`}>
+                        <Link to="/download/">Download</Link>
+                        <Link to="/firestick/">Firestick</Link>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import Layout from '../c/Layout'
+import Seo from '../c/seo'
 
 export default function Index() {
-  return (
-    <div>
-      <h1>Cyberflix</h1>
-      <p className="text-red-500">
-        hello
-      </p>
-    </div>
-  )
+	return (
+		<Layout>
+			<Seo title="" />
+			<article className="bg-green-400 py-12">
+				<div className="container mx-auto">
+					<h2>Cyberflix TV</h2>
+                    <button className="btn-alpha">Download Now</button>
+				</div>
+			</article>
+			<article className="py-12 container mx-auto">
+				<h2>Cyberflix Features</h2>
+				<p>Android App store. Below shown some of the features bundled with this app. You will experience more features when you use this app. Every day new games and apps get added to store and you will have access to those latest apps and games for free. Download this app store and stop relying on web for downloading mods and other apps. Because downloading all your Android apps using a app store is much more safer than downloading them using web. Because there is a high chance of downloading scam software. Best acmarket play store alternative.</p>
+			</article>
+		</Layout>
+	)
 }
